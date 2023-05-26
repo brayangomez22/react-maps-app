d298151735d7d6bf482ccf20de67301cd78ee40c
Brayan Gómez <brayan.gomez@sofka.com.co>
2023-05-26 08:04:53 -0500

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+/* eslint import/no-webpack-loader-syntax: off */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { MapsApp } from "./MapsApp";
+
+//@ts-ignore
+import mapboxgl from "!mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja3hramV2OWIwbjEwMzFwYzJlZWl6N2g5In0.iKXPpYvo7UPRiiZ-x_lCrw";
+
+if (!navigator.geolocation) {
+  alert("Tu navegador no tiene opción de Geolocation");
+  throw new Error("Tu navegador no tiene opción de Geolocation");
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MapsApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
